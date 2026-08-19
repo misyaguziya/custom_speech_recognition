50a30bf13dc511915241d8ffa9647ed8b47d5cee
misyaguziya <53165965+misyaguziya@users.noreply.github.com>
2026-08-19 13:51:23 +0900

    👍️[Update] version 3.10.4.2 -> 3.10.4.3
    VRCT向けの安定化修正2件 (ce9e62d, c8defcc) を含むバージョン。
    既存の 3.10.4.2 タグは df4cedf/9ebd60a 時点のコミットを指しており
    今回の修正を含まないため、新しいタグを切る。

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -27,7 +27,7 @@ except (ModuleNotFoundError, ImportError):
     pass
 
 __author__ = "Anthony Zhang (Uberi)"
-__version__ = "3.10.4.2"
+__version__ = "3.10.4.3"
 __license__ = "BSD"
 
 from urllib.parse import urlencode
